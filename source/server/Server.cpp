d71abbc5b1fe6690888a7b5d276182bab4931c96
Jan Tusil <410062@mail.muni.cz>
2017-04-28 13:03:16 +0200

    Server: pImpl

--- source/server/Server.cpp
+++ source/server/Server.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <sstream>
+#include <set>
 
+#include <boost/asio.hpp>
 #include <boost/variant/static_visitor.hpp>
 
 #include <cereal/archives/binary.hpp>
@@ -9,153 +11,151 @@
 
 #include <entityplus/entity.h>
 
-#include <common/ServerMessage.h>
 #include <common/ClientMessage.h>
+#include <common/GameObjectManager.h>
+#include <common/IConnection.h>
 #include <common/Messages.h>
-
+#include <common/PolygonalShape.h>
+#include <common/ServerMessage.h>
 #include <common/EntityComponentSystem.h>
 
+#include <server/IBroadcaster.h>
+#include "ConnectionToClient.hpp"
+#include "GameModel.h"
+#include "PlayerManager.h"
 
 #include "Server.h"
 
 using namespace std;
 using boost::asio::ip::tcp;
 
-// TODO pImpl
-
-Server::Server(int port) :
-		acceptor(io_service, tcp::endpoint(tcp::v4(), port)),
-		gameModel(gameObjects, ecs, *this)
+class Server::Impl : private IConnection::IHandler, private IBroadcaster
 {
-	start_accept();
-	init_player_shape();
-}
-
-Server::~Server() {
-	for (ConnectionToClient * conn : connections)
-		delete conn;
-}
-
-void Server::run() {
-	io_service.run();
-}
-
-void Server::shutdown() {
-	io_service.stop();
-	// TODO implement this
-}
-
-class Server::Receiver : public boost::static_visitor<void> {
 public:
-	explicit Receiver(Server & self, ConnectionToClient & connection) :
-	self(self),
-	connection(connection)
+	explicit Impl(int port) :
+	acceptor(io_service, tcp::endpoint(tcp::v4(), port)),
+	gameModel(gameObjects, ecs, *this)
 	{
-
+		start_accept();
+		init_player_shape();
 	}
 
-	void operator()(MsgIntroduceMyPlayer const /*& msg*/) {
-
+	~Impl() {
+		for (ConnectionToClient * conn : connections)
+			delete conn;
 	}
 
-	void operator()(MsgPlayerMovesLeft const & /*msg*/) {
-		self.playerMoves(connection, Player::Movement::Left);
+	void run() {
+		io_service.run();
 	}
 
-	void operator()(MsgPlayerMovesRight const & /*msg*/) {
-		self.playerMoves(connection, Player::Movement::Right);
+	void shutdown() {
+		io_service.stop();
+		// TODO implement this
 	}
-
-	void operator()(MsgPlayerMovesForward const & /*msg*/) {
-		self.playerMoves(connection, Player::Movement::Forward);
-	}
-
-	void operator()(MsgPlayerMovesBackward const & /*msg*/) {
-		self.playerMoves(connection, Player::Movement::Backward);
-	}
-
-	void operator()(MsgPlayerStops const & /*msg*/) {
-		self.playerMoves(connection, Player::Movement::None);
-	}
-
 
 private:
-	Server & self;
-	ConnectionToClient & connection;
-};
 
-
-void Server::received(ConnectionToClient & connection, ClientMessage msg) {
-	Receiver receiver{*this, connection};
-	boost::apply_visitor(receiver, msg.data);
-}
-
-
-void Server::received(IConnection & connection, Message msg) {
-	auto & conn = dynamic_cast<ConnectionToClient &>(connection);
-	cout << "Server received message from " << conn.socket().remote_endpoint() << endl;
-	switch(msg.tag) {
-	case Tag::UniversalClientMessage:
-		return received(conn, ClientMessage::from(msg));
-
-	default:
-		break;
-	}
-}
-
-void Server::playerMoves(ConnectionToClient & client, Player::Movement movement) {
-	auto it = connection2entity.find(&client);
-	if (it == connection2entity.end()) {
-		cerr << "[warn] player does not exist, but moves" << endl;
-		return;
+	class Receiver;
+	void received(ConnectionToClient & connection, ClientMessage msg) {
+		Receiver receiver{*this, connection};
+		boost::apply_visitor(receiver, msg.data);
 	}
 
-	entity_t player = it->second;
-	//Player & player = *connection2player[&client];
-	//entity_t player = *connection2entity[&client];
-	player.sync();
-	if (player.has_component<Position>()) {
-		auto newSpeed = toVector(movement);
-		cout << "New speed: " << newSpeed << endl;
-		player.get_component<Position>().speed = newSpeed;
+	class Receiver : public boost::static_visitor<void> {
+	public:
+		explicit Receiver(Impl & self, ConnectionToClient & connection) :
+		self(self),
+		connection(connection)
+		{
+
+		}
+
+		void operator()(MsgIntroduceMyPlayer const /*& msg*/) {
+
+		}
+
+		void operator()(MsgPlayerMovesLeft const & /*msg*/) {
+			self.playerMoves(connection, Player::Movement::Left);
+		}
+
+		void operator()(MsgPlayerMovesRight const & /*msg*/) {
+			self.playerMoves(connection, Player::Movement::Right);
+		}
+
+		void operator()(MsgPlayerMovesForward const & /*msg*/) {
+			self.playerMoves(connection, Player::Movement::Forward);
+		}
+
+		void operator()(MsgPlayerMovesBackward const & /*msg*/) {
+			self.playerMoves(connection, Player::Movement::Backward);
+		}
+
+		void operator()(MsgPlayerStops const & /*msg*/) {
+			self.playerMoves(connection, Player::Movement::None);
+		}
+
+	private:
+		Impl & self;
+		ConnectionToClient & connection;
+	};
+
+	/* < IConnection::IHandler > */
+	void received(IConnection & connection, Message msg) override {
+		auto & conn = dynamic_cast<ConnectionToClient &>(connection);
+		cout << "Server received message from " << conn.socket().remote_endpoint() << endl;
+		switch(msg.tag) {
+		case Tag::UniversalClientMessage:
+			return received(conn, ClientMessage::from(msg));
+
+		default:
+			break;
+		}
 	}
 
+	void disconnected(IConnection & connection) override {
+		auto & conn = dynamic_cast<ConnectionToClient &>(connection);
+		cout << "Client " << conn.socket().remote_endpoint() << " disconnected." << endl;
 
-	//player.gameObject().center += toVector(movement);
-	//broadcast(createMessage_NewObjectPosition(player.gameObject()));
-}
-
-Vector Server::toVector(Player::Movement movement) {
-	switch(movement) {
-	case Player::Movement::Left:
-		return {-5, 0};
-	case Player::Movement::Right:
-		return {+5, 0};
-	case Player::Movement::Backward:
-		return {0, -5};
-	case Player::Movement::Forward:
-		return {0, +15};
-
-	case Player::Movement::None:
-		return {0, 0};
-	default:
-		return {0, 0}; // exception might be nicer
+		connections.erase(&conn);
 	}
-}
+	/* </IConnection::IHandler > */
 
-void Server::disconnected(IConnection & connection) {
-	auto & conn = dynamic_cast<ConnectionToClient &>(connection);
-	cout << "Client " << conn.socket().remote_endpoint() << " disconnected." << endl;
+	void send(ConnectionToClient & client, ServerMessage const & msg) {
+		auto m = msg.to_message();
+		client.send(m);
 
-	connections.erase(&conn);
-}
+	}
+	void broadcast(ServerMessage const & msg) {
+		broadcast(msg.to_message());
+	}
+	/* < IBroadcaster > */
+	void broadcast(Message msg) override {
+		// A copy of message is created for every connection,
+		// which is not really optimal. We may optimize that later.
+		for (ConnectionToClient * cl : connections) {
+			cl->send(msg);
+		}
 
-void Server::start_accept() {
-	auto conn = new ConnectionToClient(acceptor.get_io_service());
+	}
+	void notify(GameObject const & gameObject) override {
+		broadcast(createMessage_NewObjectPosition(gameObject));
+	}
+	void notify(entity_t const & entity, AnyComponent const &component) override {
+		MsgUpdateEntity mue;
+		mue.entity_id = entity.get_component<EntityID>();
+		mue.components = {component};
+		ServerMessage usm{mue};
+		broadcast(usm);
+	}
+	/* </IBroadcaster > */
 
-	// Capturing unique pointers is not easy
-	acceptor.async_accept(conn->socket(),
-			[this, conn](const boost::system::error_code & error){
+	void start_accept() {
+		auto conn = new ConnectionToClient(acceptor.get_io_service());
+
+		// Capturing unique pointers is not easy
+		acceptor.async_accept(conn->socket(),
+				[this, conn](const boost::system::error_code & error){
 
 			if (!error)
 			{
@@ -170,131 +170,158 @@ void Server::start_accept() {
 			}
 
 			start_accept();
-	});
-}
+		});
+	}
 
-void Server::init_player_shape() {
-	playerShape.clear();
-	playerShape.push_back({-10, 0});
-	playerShape.push_back({-20, -10});
-	playerShape.push_back({+20, -10});
-	playerShape.push_back({+10, 0});
-}
+	void newClientConnected(ConnectionToClient & client) {
+		client.send(Message{Tag::Hello, {1,2,3}});
+		//send_him_a_few_polygons(client);
+
+		// TODO create a new player. We will need some manager of players.
+		uint32_t const object_id = gameObjects.get_fresh_id();
+		auto _object = make_unique<GameObject>(object_id);
+		GameObject & object = *_object;
+		gameObjects.insert(std::move(_object));
+		object.shape = playerShape;
+		object.center = {Scalar(rand() % 200), Scalar(rand() % 200)};
+
+		auto const player_id = players.get_fresh_id();
+		auto _player = make_unique<Player>(player_id, object);
+		Player & player = *_player;
+		players.insert(std::move(_player));
+		//connection2player[&client] = &player;
+
+		// Send diffs to all current players.
+		// New player must not be in the set yet.
+		broadcast(createMessage_NewGameObject(object));
+		broadcast(createMessage_NewPlayer(player));
+
+		// Send whole game state to the new player
+		for (GameObject const & object : gameObjects) {
+			send(client, createMessage_NewGameObject(object));
+		}
+
+		for (Player const & player : players) {
+			send(client, createMessage_NewPlayer(player));
+		}
+
+		// Some experiments with EntityComponent
+		// Opravdu bych mohl pridat komponentu pro identitu entit...?
+		entity_t entity = ecs.entityManager.create_entity(EntityID::newID());
+		Position pos;
+		pos.center = object.center;
+		entity.add_component<Position>(pos);
+		//entity.add_component<Shape>(CircleShape{5.1}); // for some reazon it is zero on client
+		entity.add_component<Shape>(playerShape);
+		MsgNewEntity msg;
+		connection2entity[&client] = entity;
+		msg.components = EntityComponentSystem::all_components(entity);
+		msg.entity_id = entity.get_component<EntityID>();
+		send(client, {msg});
+		broadcast({msg});
+
+		// Taky musime mit PlayerComponent
+		// Budeme mit zpravu, ktera umi rict 'Nova entita' a seznam komponent.
+		// My chceme updatovat
+	}
+
+	static Vector toVector(Player::Movement movement) {
+		switch(movement) {
+		case Player::Movement::Left:
+			return {-5, 0};
+		case Player::Movement::Right:
+			return {+5, 0};
+		case Player::Movement::Backward:
+			return {0, -5};
+		case Player::Movement::Forward:
+			return {0, +15};
+
+		case Player::Movement::None:
+			return {0, 0};
+		default:
+			return {0, 0}; // exception might be nicer
+		}
+	}
+
+	static ServerMessage createMessage_NewGameObject(GameObject const & object) {
+		MsgNewPolygonalObject npo;
+		npo.object_id = object.id();
+		npo.center = object.center;
+		npo.shape = object.shape;
+		return ServerMessage {npo};
+	}
+
+	static ServerMessage createMessage_NewObjectPosition(GameObject const & object) {
+		MsgObjectPosition mop;
+		mop.object_id = object.id();
+		mop.new_center = object.center;
+		return ServerMessage {mop};
+	}
+
+	static ServerMessage createMessage_NewPlayer(Player const & player) {
+		MsgNewPlayer mnp;
+		mnp.player_id = player.id();
+		mnp.object_id = player.gameObject().id();
+		mnp.playerName = player.name;
+		return ServerMessage {mnp};
+	}
+
+	void playerMoves(ConnectionToClient & client, Player::Movement movement) {
+		auto it = connection2entity.find(&client);
+		if (it == connection2entity.end()) {
+			cerr << "[warn] player does not exist, but moves" << endl;
+			return;
+		}
+
+		entity_t player = it->second;
+		//Player & player = *connection2player[&client];
+		//entity_t player = *connection2entity[&client];
+		player.sync();
+		if (player.has_component<Position>()) {
+			auto newSpeed = toVector(movement);
+			cout << "New speed: " << newSpeed << endl;
+			player.get_component<Position>().speed = newSpeed;
+		}
+
+
+		//player.gameObject().center += toVector(movement);
+		//broadcast(createMessage_NewObjectPosition(player.gameObject()));
+	}
+
+	void init_player_shape() {
+		playerShape.clear();
+		playerShape.push_back({-10, 0});
+		playerShape.push_back({-20, -10});
+		playerShape.push_back({+20, -10});
+		playerShape.push_back({+10, 0});
+	}
+
+	boost::asio::io_service io_service;
+	boost::asio::ip::tcp::acceptor acceptor;
+
+	// We do not use smart pointers to ease removal of items from set
+	std::set<ConnectionToClient *> connections;
+	//std::map<ConnectionToClient const *, Player *> connection2player;
+	std::map<ConnectionToClient const *, entity_t> connection2entity;
+	GameObjectManager gameObjects;
+	PlayerManager players;
+	PolygonalShape playerShape;
+	EntityComponentSystem ecs;
+	GameModel gameModel;
+};
 
 // TODO we will need to have some area size.
 
-void Server::newClientConnected(ConnectionToClient & client) {
-	client.send(Message{Tag::Hello, {1,2,3}});
-	//send_him_a_few_polygons(client);
-
-	// TODO create a new player. We will need some manager of players.
-	uint32_t const object_id = gameObjects.get_fresh_id();
-	auto _object = make_unique<GameObject>(object_id);
-	GameObject & object = *_object;
-	gameObjects.insert(std::move(_object));
-	object.shape = playerShape;
-	object.center = {Scalar(rand() % 200), Scalar(rand() % 200)};
-
-	auto const player_id = players.get_fresh_id();
-	auto _player = make_unique<Player>(player_id, object);
-	Player & player = *_player;
-	players.insert(std::move(_player));
-	//connection2player[&client] = &player;
-
-	// Send diffs to all current players.
-	// New player must not be in the set yet.
-	broadcast(createMessage_NewGameObject(object));
-	broadcast(createMessage_NewPlayer(player));
-
-	// Send whole game state to the new player
-	for (GameObject const & object : gameObjects) {
-		send(client, createMessage_NewGameObject(object));
-	}
-
-	for (Player const & player : players) {
-		send(client, createMessage_NewPlayer(player));
-	}
-
-	// Some experiments with EntityComponent
-	// Opravdu bych mohl pridat komponentu pro identitu entit...?
-	entity_t entity = ecs.entityManager.create_entity(EntityID::newID());
-	Position pos;
-	pos.center = object.center;
-	entity.add_component<Position>(pos);
-	//entity.add_component<Shape>(CircleShape{5.1}); // for some reazon it is zero on client
-	entity.add_component<Shape>(playerShape);
-	MsgNewEntity msg;
-	connection2entity[&client] = entity;
-	msg.components = EntityComponentSystem::all_components(entity);
-	msg.entity_id = entity.get_component<EntityID>();
-	send(client, {msg});
-	broadcast({msg});
-
-	// Taky musime mit PlayerComponent
-	// Budeme mit zpravu, ktera umi rict 'Nova entita' a seznam komponent.
-	// My chceme updatovat
+Server::Server(int port) {
+	pImpl = make_unique<Impl>(port);
 }
 
-ServerMessage Server::createMessage_NewGameObject(GameObject const & object) {
-	MsgNewPolygonalObject npo;
-	npo.object_id = object.id();
-	npo.center = object.center;
-	npo.shape = object.shape;
-	return ServerMessage {npo};
+Server::~Server() = default;
+
+void Server::run() {
+	pImpl->run();
 }
 
-ServerMessage Server::createMessage_NewObjectPosition(GameObject const & object) {
-	MsgObjectPosition mop;
-	mop.object_id = object.id();
-	mop.new_center = object.center;
-	return ServerMessage {mop};
+void Server::shutdown() {
+	pImpl->shutdown();
 }
-
-ServerMessage Server::createMessage_NewPlayer(Player const & player) {
-	MsgNewPlayer mnp;
-	mnp.player_id = player.id();
-	mnp.object_id = player.gameObject().id();
-	mnp.playerName = player.name;
-	return ServerMessage {mnp};
-}
-
-void Server::send(ConnectionToClient & client, ServerMessage const & msg) {
-	auto m = msg.to_message();
-	client.send(m);
-}
-
-void Server::broadcast(Message msg) {
-	// A copy of message is created for every connection,
-	// which is not really optimal. We may optimize that later.
-	for (ConnectionToClient * cl : connections) {
-		cl->send(msg);
-	}
-}
-
-void Server::send_him_a_few_polygons(ConnectionToClient & client) {
-	MsgNewPolygonalObject npo;
-	npo.object_id = 25;
-	npo.shape.push_back(Point{40, 40});
-	npo.shape.push_back(Point{40, 120});
-	npo.shape.push_back(Point{70, 140});
-	npo.shape.push_back(Point{140,140});
-	ServerMessage usm{npo};
-	client.send(usm.to_message());
-}
-
-void Server::notify(GameObject const & gameObject) {
-	broadcast(createMessage_NewObjectPosition(gameObject));
-}
-
-void Server::notify(entity_t const &entity, AnyComponent const &component) {
-	MsgUpdateEntity mue;
-	mue.entity_id = entity.get_component<EntityID>();
-	mue.components = {component};
-	ServerMessage usm{mue};
-	broadcast(usm);
-}
-
-void Server::broadcast(ServerMessage const & msg) {
-	broadcast(msg.to_message());
-}
-
--- source/server/Server.h
+++ source/server/Server.h
@@ -1,22 +1,8 @@
 #pragma once
 
-#include <set>
-#include <boost/asio.hpp>
+#include <memory>
 
-#include <common/IConnection.h>
-#include <common/Messages.h>
-#include <common/GameObjectManager.h>
-#include <common/PolygonalShape.h>
-#include <server/IBroadcaster.h>
-#include "ConnectionToClient.hpp"
-#include "GameModel.h"
-#include "PlayerManager.h"
-
-struct ServerMessage;
-struct ClientMessage;
-
-
-class Server : private IConnection::IHandler, private IBroadcaster
+class Server
 {
 	public:
 		explicit Server(int port);
@@ -26,46 +12,6 @@ class Server : private IConnection::IHandler, private IBroadcaster
 		void shutdown();
 
 	private:
-		class Receiver;
-		friend class Server::Receiver;
-
-		void received(ConnectionToClient & connection, ClientMessage msg);
-		/* < IConnection::IHandler > */
-		void received(IConnection & connection, Message msg) override;
-		void disconnected(IConnection & connection) override;
-		/* < IConnection::IHandler > */
-
-		void send(ConnectionToClient & client, ServerMessage const & msg);
-		void broadcast(ServerMessage const & msg);
-		/* < IBroadcaster > */
-		void broadcast(Message msg) override;
-		void notify(GameObject const & gameObject) override;
-		void notify(entity_t const & entity, AnyComponent const &component) override;
-		/* </IBroadcaster > */
-
-		void start_accept();
-		void newClientConnected(ConnectionToClient & client);
-
-		static Vector toVector(Player::Movement movement);
-		static ServerMessage createMessage_NewGameObject(GameObject const & object);
-		static ServerMessage createMessage_NewObjectPosition(GameObject const & object);
-		static ServerMessage createMessage_NewPlayer(Player const & player);
-		void playerMoves(ConnectionToClient & client, Player::Movement movement);
-		void send_him_a_few_polygons(ConnectionToClient & client);
-		void init_player_shape();
-
-		boost::asio::io_service io_service;
-		boost::asio::ip::tcp::acceptor acceptor;
-
-		// We do not use smart pointers to ease removal of items from set
-		std::set<ConnectionToClient *> connections;
-		//std::map<ConnectionToClient const *, Player *> connection2player;
-		std::map<ConnectionToClient const *, entity_t> connection2entity;
-		GameObjectManager gameObjects;
-		PlayerManager players;
-		PolygonalShape playerShape;
-		EntityComponentSystem ecs;
-		GameModel gameModel;
+		class Impl;
+		std::unique_ptr<Impl> pImpl;
 };
-
-
